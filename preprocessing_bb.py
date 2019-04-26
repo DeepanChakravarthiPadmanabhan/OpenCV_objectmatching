547ff7109f6781816a41cce5ad903a4267b8a599
njanirudh <anijaya9@gmail.com>
2019-04-26 14:27:49 +0200

    Changes to thresholding

--- preprocessing_bb.py
+++ preprocessing_bb.py
@@ -2,16 +2,18 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-def get_object(img):
+def get_object(input):
 
     BLACK_THRESHOLD = 200
     THIN_THRESHOLD = 10
 
-    img = cv2.GaussianBlur(img, (5, 5), 0)
+    img = cv2.GaussianBlur(input, (5, 5), 0)
     imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    ret, thresh = cv2.threshold(imgray, 130, 55, 0)
+    ret, thresh = cv2.threshold(imgray,0,255,cv2.THRESH_TRIANGLE)
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    cv2.imshow('thresh', thresh)
 
     idx = 0
     for cnt in contours:
@@ -29,6 +31,7 @@ if __name__ == "__main__":
 
 
     img = cv2.imread('image_tech/Q3.jpeg')
+    img = cv2.resize(img,(640,480))
     out = get_object(img)
 
     cv2.imshow('Output', out)
